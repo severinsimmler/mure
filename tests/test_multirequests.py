d811aeb4d97e1322e043bd78b40e1f6838e55f82
severinsimmler <s.simmler@snapaddy.com>
2023-01-12 16:16:22 +0100

    chore: unit tests

---
+++ tests/test_multirequests.py
@@ -0,0 +1,57 @@
+import pytest
+
+import multirequests
+
+
+def test_get():
+    resources = [
+        {"url": "https://httpbin.org/get"},
+        {"url": "https://httpbin.org/get", "params": {"foo": "bar"}},
+        {"url": "invalid"},
+    ]
+
+    responses = list(multirequests.get(resources, batch_size=2))
+
+    assert len(responses) == 3
+    assert responses[0]["ok"]
+    assert responses[1]["ok"]
+    assert not responses[2]["ok"]
+
+
+def test_post():
+    resources = [
+        {"url": "https://httpbin.org/post"},
+        {"url": "https://httpbin.org/post", "json": {"foo": "bar"}},
+        {"url": "invalid"},
+    ]
+
+    responses = list(multirequests.post(resources, batch_size=2))
+
+    assert len(responses) == 3
+    assert responses[0]["ok"]
+    assert responses[1]["ok"]
+    assert not responses[2]["ok"]
+
+
+def test_mixed():
+    resources = [
+        {"method": "GET", "url": "https://httpbin.org/get"},
+        {"method": "GET", "url": "https://httpbin.org/get", "params": {"foo": "bar"}},
+        {"method": "POST", "url": "https://httpbin.org/post"},
+        {"method": "POST", "url": "https://httpbin.org/post", "json": {"foo": "bar"}},
+        {"method": "GET", "url": "invalid"},
+    ]
+
+    responses = list(multirequests.request(resources, batch_size=2))
+
+    assert len(responses) == 5
+    assert responses[0]["ok"]
+    assert responses[1]["ok"]
+    assert responses[2]["ok"]
+    assert responses[3]["ok"]
+    assert not responses[4]["ok"]
+
+
+def test_missing_method():
+    with pytest.raises(KeyError):
+        list(multirequests.request([{"url": "https://httpbin.org/get"}]))
