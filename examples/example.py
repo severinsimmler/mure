da167b7ebaaf1d7cdbe4834f097466dd29033fbe
severinsimmler <s.simmler@snapaddy.com>
2023-09-19 15:58:15 +0200

    feat: example

---
+++ examples/example.py
@@ -0,0 +1,22 @@
+import mure
+from mure.dtos import Resource
+from mure.iterator import LOGGER
+
+LOGGER.set_level("DEBUG")
+
+resources: list[Resource] = [
+    {"url": "https://google.com"},
+    {"url": "https://youtube.com"},
+    {"url": "https://facebook.com"},
+    {"url": "https://instagram.com"},
+    {"url": "https://twitter.com"},
+    {"url": "https://baidu.com"},
+    {"url": "https://wikipedia.org"},
+    {"url": "https://yahoo.com"},
+    {"url": "https://yandex.ru"},
+    {"url": "https://whatsapp.com"},
+]
+
+
+for response in mure.get(resources, batch_size=5, log_errors=False):
+    pass
