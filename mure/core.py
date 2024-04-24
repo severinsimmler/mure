3d4a700f5a080f83d8c1c3f4e5a31bf33b2dd674
severinsimmler <s.simmler@snapaddy.com>
2024-04-24 21:27:38 +0200

    feat: high-level functinos

--- mure/core.py
+++ mure/core.py
@@ -1,40 +1,6 @@
 from mure.cache import Cache
-from mure.dtos import DELETE, GET, HEAD, PATCH, POST, PUT, HTTPMethod, HTTPResource, Resource
 from mure.iterator import ResponseIterator
-
-
-def request(
-    resources: list[HTTPResource],
-    *,
-    batch_size: int = 5,
-    cache: Cache | None = None,
-) -> ResponseIterator:
-    """Perform HTTP request for each resource in the given batch.
-
-    Parameters
-    ----------
-    resources : list[HTTPResource]
-        Resources to request.
-    batch_size : int
-        Number of resources to request concurrently, by default 5.
-    cache : Cache | None, optional
-        Cache to use for storing responses, by default None.
-
-    Returns
-    -------
-    ResponseIterator
-        The server's responses for each resource.
-    """
-    if not isinstance(resources, list):
-        raise TypeError(f"Expected list of resources, got {type(resources)}")
-
-    if any("method" not in resource for resource in resources):
-        raise KeyError("Missing HTTP method in resource")
-
-    if any("url" not in resource for resource in resources):
-        raise KeyError("Missing URL in resource")
-
-    return ResponseIterator(resources, batch_size=batch_size, cache=cache)
+from mure.models import Request, Resource
 
 
 def get(
@@ -59,7 +25,11 @@ def get(
     ResponseIterator
         The server's responses for each resource.
     """
-    return _request(GET, resources, batch_size=batch_size, cache=cache)
+    return ResponseIterator(
+        [Request("GET", **resource) for resource in resources],
+        batch_size=batch_size,
+        cache=cache,
+    )
 
 
 def post(
@@ -84,7 +54,11 @@ def post(
     ResponseIterator
         The server's responses for each resource.
     """
-    return _request(POST, resources, batch_size=batch_size, cache=cache)
+    return ResponseIterator(
+        [Request("POST", **resource) for resource in resources],
+        batch_size=batch_size,
+        cache=cache,
+    )
 
 
 def put(
@@ -109,7 +83,11 @@ def put(
     ResponseIterator
         The server's responses for each resource.
     """
-    return _request(PUT, resources, batch_size=batch_size, cache=cache)
+    return ResponseIterator(
+        [Request("PUT", **resource) for resource in resources],
+        batch_size=batch_size,
+        cache=cache,
+    )
 
 
 def patch(
@@ -134,7 +112,11 @@ def patch(
     ResponseIterator
         The server's responses for each resource.
     """
-    return _request(PATCH, resources, batch_size=batch_size, cache=cache)
+    return ResponseIterator(
+        [Request("PATCH", **resource) for resource in resources],
+        batch_size=batch_size,
+        cache=cache,
+    )
 
 
 def delete(
@@ -159,7 +141,11 @@ def delete(
     ResponseIterator
         The server's responses for each resource.
     """
-    return _request(DELETE, resources, batch_size=batch_size, cache=cache)
+    return ResponseIterator(
+        [Request("DELETE", **resource) for resource in resources],
+        batch_size=batch_size,
+        cache=cache,
+    )
 
 
 def head(
@@ -184,36 +170,8 @@ def head(
     ResponseIterator
         The server's responses for each resource.
     """
-    return _request(HEAD, resources, batch_size=batch_size, cache=cache)
-
-
-def _request(
-    method: HTTPMethod,
-    resources: list[Resource],
-    *,
-    batch_size: int = 5,
-    cache: Cache | None = None,
-) -> ResponseIterator:
-    """Perform a HTTP request using the specified method for each resource.
-
-    Parameters
-    ----------
-    method : HTTPMethod
-        HTTP method to use.
-    resources : list[HTTPResource]
-        Resources to request.
-    batch_size : int
-        Number of resources to request concurrently, by default 5.
-    cache : Cache | None, optional
-        Cache to use for storing responses, by default None.
-
-    Returns
-    -------
-    ResponseIterator
-        The server's responses for each resource.
-    """
-    return request(
-        [{**resource, "method": method} for resource in resources],
+    return ResponseIterator(
+        [Request("HEAD", **resource) for resource in resources],
         batch_size=batch_size,
         cache=cache,
     )
