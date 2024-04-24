4fa2111c5fea170b1c0bbd6bf777262cbbec517e
severinsimmler <s.simmler@snapaddy.com>
2024-04-24 21:28:34 +0200

    feat: high-level functinos

--- mure/core.py
+++ mure/core.py
@@ -3,6 +3,35 @@ from mure.iterator import ResponseIterator
 from mure.models import Request, Resource
 
 
+def delete(
+    resources: list[Resource],
+    *,
+    batch_size: int = 5,
+    cache: Cache | None = None,
+) -> ResponseIterator:
+    """Perform a DELETE request for each resource.
+
+    Parameters
+    ----------
+    resources : list[Resource]
+        Resources to request.
+    batch_size : int
+        Number of items to request per batch concurrently, by default 5.
+    cache : Cache | None, optional
+        Cache to use for storing responses, by default None.
+
+    Returns
+    -------
+    ResponseIterator
+        The server's responses for each resource.
+    """
+    return ResponseIterator(
+        [Request("DELETE", **resource) for resource in resources],
+        batch_size=batch_size,
+        cache=cache,
+    )
+
+
 def get(
     resources: list[Resource],
     *,
@@ -27,6 +56,64 @@ def get(
     """
     return ResponseIterator(
         [Request("GET", **resource) for resource in resources],
+        batch_size=batch_size,
+        cache=cache,
+    )
+
+
+def head(
+    resources: list[Resource],
+    *,
+    batch_size: int = 5,
+    cache: Cache | None = None,
+) -> ResponseIterator:
+    """Perform a HEAD request for each resource.
+
+    Parameters
+    ----------
+    resources : list[Resource]
+        Resources to request.
+    batch_size : int
+        Number of items to request per batch concurrently, by default 5.
+    cache : Cache | None, optional
+        Cache to use for storing responses, by default None.
+
+    Returns
+    -------
+    ResponseIterator
+        The server's responses for each resource.
+    """
+    return ResponseIterator(
+        [Request("HEAD", **resource) for resource in resources],
+        batch_size=batch_size,
+        cache=cache,
+    )
+
+
+def patch(
+    resources: list[Resource],
+    *,
+    batch_size: int = 5,
+    cache: Cache | None = None,
+) -> ResponseIterator:
+    """Perform a PATCH request for each resource.
+
+    Parameters
+    ----------
+    resources : list[Resource]
+        Resources to request.
+    batch_size : int
+        Number of items to request per batch concurrently, by default 5.
+    cache : Cache | None, optional
+        Cache to use for storing responses, by default None.
+
+    Returns
+    -------
+    ResponseIterator
+        The server's responses for each resource.
+    """
+    return ResponseIterator(
+        [Request("PATCH", **resource) for resource in resources],
         batch_size=batch_size,
         cache=cache,
     )
@@ -85,93 +172,6 @@ def put(
     """
     return ResponseIterator(
         [Request("PUT", **resource) for resource in resources],
-        batch_size=batch_size,
-        cache=cache,
-    )
-
-
-def patch(
-    resources: list[Resource],
-    *,
-    batch_size: int = 5,
-    cache: Cache | None = None,
-) -> ResponseIterator:
-    """Perform a PATCH request for each resource.
-
-    Parameters
-    ----------
-    resources : list[Resource]
-        Resources to request.
-    batch_size : int
-        Number of items to request per batch concurrently, by default 5.
-    cache : Cache | None, optional
-        Cache to use for storing responses, by default None.
-
-    Returns
-    -------
-    ResponseIterator
-        The server's responses for each resource.
-    """
-    return ResponseIterator(
-        [Request("PATCH", **resource) for resource in resources],
-        batch_size=batch_size,
-        cache=cache,
-    )
-
-
-def delete(
-    resources: list[Resource],
-    *,
-    batch_size: int = 5,
-    cache: Cache | None = None,
-) -> ResponseIterator:
-    """Perform a DELETE request for each resource.
-
-    Parameters
-    ----------
-    resources : list[Resource]
-        Resources to request.
-    batch_size : int
-        Number of items to request per batch concurrently, by default 5.
-    cache : Cache | None, optional
-        Cache to use for storing responses, by default None.
-
-    Returns
-    -------
-    ResponseIterator
-        The server's responses for each resource.
-    """
-    return ResponseIterator(
-        [Request("DELETE", **resource) for resource in resources],
-        batch_size=batch_size,
-        cache=cache,
-    )
-
-
-def head(
-    resources: list[Resource],
-    *,
-    batch_size: int = 5,
-    cache: Cache | None = None,
-) -> ResponseIterator:
-    """Perform a HEAD request for each resource.
-
-    Parameters
-    ----------
-    resources : list[Resource]
-        Resources to request.
-    batch_size : int
-        Number of items to request per batch concurrently, by default 5.
-    cache : Cache | None, optional
-        Cache to use for storing responses, by default None.
-
-    Returns
-    -------
-    ResponseIterator
-        The server's responses for each resource.
-    """
-    return ResponseIterator(
-        [Request("HEAD", **resource) for resource in resources],
         batch_size=batch_size,
         cache=cache,
     )
