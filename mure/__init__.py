57b989ce4c0de8a0359921d8e7d7687e77a40800
severinsimmler <s.simmler@snapaddy.com>
2024-04-24 21:09:28 +0200

    feat: imports

--- mure/__init__.py
+++ mure/__init__.py
@@ -1,1 +1,6 @@
-from mure.core import delete, get, head, patch, post, put, request
+from mure.core import delete as delete
+from mure.core import get as get
+from mure.core import head as head
+from mure.core import patch as patch
+from mure.core import post as post
+from mure.core import put as put
