691467a263cdd8b32ba91fcb44ca4b035e1e401b
severinsimmler <s.simmler@snapaddy.com>
2025-12-19 14:30:59 +0100

    feat: new caching

--- examples/example.py
+++ examples/example.py
@@ -1,4 +1,5 @@
 import mure
+from mure.cache import Cache
 from mure.iterator import LOGGER
 from mure.models import Resource
 
@@ -16,10 +17,10 @@ resources: list[Resource] = [
 ]
 
 print("START FIRST RUN")
-for i, response in enumerate(mure.get(resources, batch_size=5, enable_cache=True)):
+for i, response in enumerate(mure.get(resources, batch_size=5, cache=Cache.FILE)):
     LOGGER.info(f"Received response {i} from {response.url}: {response.status}")
 
 print("\nSTART SECOND RUN")
 # super fast, because responses are cached
-for i, response in enumerate(mure.get(resources, batch_size=5, enable_cache=True)):
+for i, response in enumerate(mure.get(resources, batch_size=5, cache=Cache.FILE)):
     LOGGER.info(f"Received response {i} from {response.url}: {response.status}")
