b68d3dbb8e7415dfc59ae25a1d6e2590080ce2e8
severinsimmler <s.simmler@snapaddy.com>
2026-06-03 15:34:42 +0200

    chore: memory cache

--- examples/example.py
+++ examples/example.py
@@ -17,10 +17,10 @@ resources: list[Resource] = [
 ]
 
 print("START FIRST RUN")
-for i, response in enumerate(mure.get(resources, batch_size=5, cache=Cache.FILE)):
+for i, response in enumerate(mure.get(resources, batch_size=5, cache=Cache.MEMORY)):
     LOGGER.info(f"Received response {i} from {response.url}: {response.status}")
 
 print("\nSTART SECOND RUN")
 # super fast, because responses are cached
-for i, response in enumerate(mure.get(resources, batch_size=5, cache=Cache.FILE)):
+for i, response in enumerate(mure.get(resources, batch_size=5, cache=Cache.MEMORY)):
     LOGGER.info(f"Received response {i} from {response.url}: {response.status}")
